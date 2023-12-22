c55c33d3d2e3248fdc47bbe016481ce929009138
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-22 18:36:40 +0900

    rename

--- internal/internal.py
+++ internal/internal.py
@@ -21,7 +21,7 @@ class ProcessMode(Enum):
 @dataclass
 class TensorInternalSequencedUnsqeezed:
     name : str
-    ttype : MetaTensorType = MetaTensorType.DEFAULT
+    meta_tensor_type : MetaTensorType = MetaTensorType.DEFAULT
     _tensor : torch.Tensor = field(repr=False,init=False)
     @property
     def tensor(self):
@@ -29,14 +29,14 @@ class TensorInternalSequencedUnsqeezed:
     @tensor.setter
     def tensor(self,tor_tensor : torch.Tensor) ->torch.Tensor:
         self._tensor = tor_tensor
-        if self.ttype==MetaTensorType.PARAMETER:
+        if self.meta_tensor_type==MetaTensorType.PARAMETER:
             self._tensor.requires_grad = True
         return self._tensor
 
 @dataclass
 class TensorInternalSequenced(TensorInternalSequencedUnsqeezed):
     def unsqueeze_to(self,dim):
-        ret = TensorInternalSequencedUnsqeezed(name=self.name,ttype=self.ttype)
+        ret = TensorInternalSequencedUnsqeezed(name=self.name,meta_tensor_type=self.meta_tensor_type)
         current_dim = self.tensor.dim()
         for _ in range(dim-current_dim):
             ret.tensor = self.tensor.unsqueeze(0)
@@ -52,11 +52,11 @@ class TensorInternal(TensorInternalSequenced):
         
     def __getitem__(self,key) ->TensorInternalSequenced:
         if self.axis_sequence == 0:
-            ret = TensorInternalSequenced(self.name,self.ttype)
+            ret = TensorInternalSequenced(self.name,self.meta_tensor_type)
             ret.tensor = self.tensor[key]
             return ret
         elif self.axis_sequence <0 :
-            ret = TensorInternalSequenced(self.name,self.ttype)
+            ret = TensorInternalSequenced(self.name,self.meta_tensor_type)
             ret.tensor = self.tensor
             return ret
         else:
@@ -73,7 +73,7 @@ class TensorsManagerSequenced:
     tensors : List[TensorInternal] = field(default_factory=list)
 
     def new_tensor(self,tensor:torch.Tensor,axis_sequence:int,ttype:MetaTensorType=MetaTensorType.DEFAULT,name:str=None):
-        current_ttp = TensorInternal(name=name,ttype=ttype,axis_sequence=axis_sequence)
+        current_ttp = TensorInternal(name=name,meta_tensor_type=ttype,axis_sequence=axis_sequence)
         current_ttp.tensor = tensor
         self.tensors.append(current_ttp) 
 
@@ -88,7 +88,7 @@ class TensorsManagerSequenced:
                 return current_tensor
 
     def get_all_params(self):
-        return {tensor.name :tensor.tensor for tensor in self.tensors if tensor.ttype == MetaTensorType.PARAMETER}
+        return {tensor.name :tensor.tensor for tensor in self.tensors if tensor.meta_tensor_type == MetaTensorType.PARAMETER}
 
     def get_max_dim(self):
         return max([tensor.tensor.dim() for tensor in self.tensors])
@@ -98,7 +98,7 @@ class TensorsManagerSequenced:
         return TensorsManagerSequenced([tensor.unsqueeze_to(max_dim) for tensor in self.tensors]),max_dim
 
     def get_min_sequence_length(self,type):
-        return min([len(tensor.tensor) for tensor in self.tensors if tensor.ttype == type])
+        return min([len(tensor.tensor) for tensor in self.tensors if tensor.meta_tensor_type == type])
         
 
 
